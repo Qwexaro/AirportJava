861df1ae409b5a4163e280b69a3d8990ba83d6ee
Qwexaro <Sergikstar25.gold@mail.ru>
2025-04-24 13:23:09 +0300

    main

---
+++ src/main/java/Main.java
@@ -0,0 +1,112 @@
+import library.Aircraft;
+import library.Airport;
+import library.Flight;
+import library.Terminal;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        String model = "Boeing";
+        Airport airport = Airport.getInstance();
+        System.out.println("Count all aircraft witch found model \"" + model + "\": " +
+                findCountAircraftWithModelAirbus(airport, model));
+
+//        System.out.println("\nList flights, which through two hours: ");
+//        for (Flight currentFlight : findFlightsLeavingInTheNextHours(airport, 2)) {
+//            System.out.println("\"" + currentFlight + "\"");
+//        }
+
+        System.out.println("\nThe number of parked planes in each terminal: " +
+                findMapCountParkedAircraftByTerminalName(airport));
+
+        System.out.println("\nFind the nearest flight to the specified terminal - A: " +
+                findFirstFlightArriveToTerminal(airport, "A"));
+    }
+
+    public static long findCountAircraftWithModelAirbus(Airport airport, String model) {
+        //TODO Метод должен вернуть количество самолетов указанной модели.
+        // подходят те самолеты, у которых name начинается со строки model
+        int count = 0;
+
+        for (Aircraft currentAircraft : airport.getAllAircrafts()) {
+            if (currentAircraft.getModel().contains(model)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static List<Flight> findFlightsLeavingInTheNextHours(Airport airport, int hours) {
+        //TODO Метод должен вернуть список отправляющихся рейсов в ближайшее количество часов.
+        ZonedDateTime zonedTimeNow = Instant.now().atZone(ZoneId.of("Europe/Moscow"));
+        ZonedDateTime zonedTimePlusHours = zonedTimeNow.plusHours(hours);
+
+
+        List<Flight> listFlightsLeavingInTheNextHour = new ArrayList<>();
+        for (Terminal currentTerminal : airport.getTerminals()) {
+            for (Flight currentFlight : currentTerminal.getFlights()) {
+                ZonedDateTime zonedTimeCurrentFlight = currentFlight.getDate()
+                        .atZone(ZoneId.of("Europe/Moscow"));
+                if(currentFlight.getType().equals(Flight.Type.DEPARTURE) &&
+                        (zonedTimeCurrentFlight.isEqual(zonedTimeNow) ||
+                                zonedTimeCurrentFlight.isAfter(zonedTimeNow)) &&
+                                (zonedTimeCurrentFlight.isEqual(zonedTimePlusHours) ||
+                                        zonedTimeCurrentFlight.isBefore(zonedTimePlusHours))
+
+                        ){
+                    listFlightsLeavingInTheNextHour.add(currentFlight);
+                }
+            }
+        }
+
+        return null;
+    }
+
+
+    public static Map<String, Integer> findMapCountParkedAircraftByTerminalName(Airport airport) {
+        //TODO Метод должен вернуть словарь с количеством припаркованных самолетов в каждом терминале.
+        Map<String, Integer> mapCountParkedAircraftByTerminalName = new HashMap<>();
+
+        for (Terminal currentTerminal : airport.getTerminals()) {
+            mapCountParkedAircraftByTerminalName.put(
+                    currentTerminal.getName(), currentTerminal.getParkedAircrafts().size()
+            );
+        }
+
+        return mapCountParkedAircraftByTerminalName;
+    }
+
+
+    public static Flight findFirstFlightArriveToTerminal(Airport airport, String terminalName) {
+        //TODO Найти ближайший прилет в указанный терминал.
+
+        ZonedDateTime zonedDateTimeNow = Instant.now().atZone(ZoneId.of("Europe/Moscow"));
+
+        Set<Flight> sortedFlights = new TreeSet<>();
+
+
+        for (Terminal currentTerminal : airport.getTerminals()) {
+            if (currentTerminal.getName().equals(terminalName)) {
+                for (Flight currentFlight : currentTerminal.getFlights()) {
+                    ZonedDateTime zonedTimeFlight = currentFlight.getDate().atZone(ZoneId.of("Europe/Moscow"));
+                    if (currentFlight.getType().equals(Flight.Type.ARRIVAL) &&
+                            zonedTimeFlight.isAfter(zonedDateTimeNow)) {
+                        sortedFlights.add(currentFlight);
+                    }
+                }
+            }
+        }
+
+        for (Flight currentFlightArrival : sortedFlights) {
+            return currentFlightArrival;
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/library/Flight.java
@@ -0,0 +1,78 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package library;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Flight implements Comparable<Flight> {
+    private static final ZoneId zoneId = ZoneId.systemDefault();
+    private static final DateTimeFormatter HOUR_FORMAT;
+    private final String code;
+    private final Type type;
+    private final Instant date;
+    private final Aircraft aircraft;
+
+    public Flight(String code, Type type, Instant date, Aircraft aircraft) {
+        this.code = code;
+        this.type = type;
+        this.date = date;
+        this.aircraft = aircraft;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public Type getType() {
+        return this.type;
+    }
+
+    public Instant getDate() {
+        return this.date;
+    }
+
+    public Aircraft getAircraft() {
+        return this.aircraft;
+    }
+
+    public String toString() {
+        String dateFormatted = HOUR_FORMAT.format(this.date);
+        return dateFormatted + " / " + this.code + " / " + this.type;
+    }
+
+    static {
+        HOUR_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm").withZone(zoneId);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Flight flight = (Flight) o;
+        return Objects.equals(code, flight.code) && type == flight.type && Objects.equals(date, flight.date) && Objects.equals(aircraft, flight.aircraft);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, type, date, aircraft);
+    }
+
+    @Override
+    public int compareTo(Flight o) {
+        return this.getDate().compareTo(o.getDate());
+    }
+
+    public static enum Type {
+        ARRIVAL,
+        DEPARTURE;
+
+        private Type() {
+        }
+    }
+}
